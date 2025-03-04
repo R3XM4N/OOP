a6acbbdc4a9826843353dc3585bef0c95496ab01
Rexxi <76392112+R3XM4N@users.noreply.github.com>
2025-03-04 14:17:28 +0100

    can remove mails

--- oop0/main.cpp
+++ oop0/main.cpp
@@ -18,16 +18,25 @@ class Person{
 
 class Document{
     public:
+        string name;
         Person sender;
         Person reciever;
         Document(){};
-        Document(Person docSender,Person docReciever, string docContent){
+        Document(string docName, Person docSender,Person docReciever, string docContent){
             sender = docSender;
             reciever = docReciever;
             content = docContent;
+            name= docName;
         }
         string GetContent(){
             return content;
+        }
+        string GetWhole(){
+            string temp;
+            temp = "Sender: " + sender.name + " " + sender.surname + "\n";
+            temp += "Reciever: " + reciever.name + " " + reciever.surname + "\n";
+            temp += "Content " + GetContent() + "\n";
+            return temp;
         }
     private:
         string content;
@@ -54,14 +63,8 @@ class Email{
          vector<Document> recievedDocs;
 };
 
-void PrintDocAll(Document doc){
-    cout << "Sender: " << doc.sender.name << " " << doc.sender.surname << "\n";
-    cout << "Reciever: " << doc.reciever.name << " " << doc.reciever.surname << "\n";
-    cout << "Content " << doc.GetContent() << "\n";
-}
-
-void SendDoc(Person sender, Email recieverEmail, string content ){
-    Document tempDoc(sender,recieverEmail.owner,content);
+void SendDoc(string docName, Person sender, Email recieverEmail, string content ){
+    Document tempDoc(docName,sender,recieverEmail.owner,content);
     recieverEmail.RecieveDoc(tempDoc);
 }
 
@@ -88,15 +91,33 @@ int main(){
             cout << "cl - clears command line\n";
             cout << "\e[1;34m   Mode commands:    \e[0m\n";
             cout << "msg - sets into message mode enabling to send a 'document'\n";
-            cout << "add - sets into add mode enabling to add a user or email\n";
+            cout << "add - sets into add mode enabling to add a email\n";
+            cout << "del - sets into delete mode enabling the deletion of a email\n";
             cout << "\e[1;34m   Utility:    \e[0m\n";
             cout << "ls - lists all emails\n";
         }
         else if (command == "msg"){
-
+            cout << "\e[1;33mEnter target mail 'name@domain'.\e[0m\n";
+            if (cin >> command)
+            {
+                for (int i = 0; i < emails.size(); i++)
+                {
+                    if (emails[i].GetFullName() == command)
+                    {
+                        
+                    }
+                    else if (i == emails.size()-1)
+                    {
+                        /* code */
+                    }
+                    
+                }
+            }
+            
         }
         else if (command == "add"){
             Person temp;
+            cout << "\e[1;33mEnter user and mail info.\e[0m\n";
             cout << "Name:   Surname:\n";
             if (cin >> temp.name >> temp.surname)
             {
@@ -106,9 +127,39 @@ int main(){
                 {
                     tempMail.owner = temp;
                     emails.push_back(tempMail);
+                    cout << "\e[1;33mSuccessfully added "<< tempMail.GetFullName() << "\e[0m\n";
                 }
             }
         }
+        else if (command == "del")
+        {
+            cout << "\e[1;31mType a mail you wish to delete: as 'emailname@domain' or exit via exit.\e[0m\n";
+            if (cin >> command)
+            {
+                if (command == "exit")
+                {
+                    cout << "\e[1;31mDelete operation exited.\e[0m\n";
+                }
+                else                
+                {
+                    for (int i = 0; i < emails.size(); i++)
+                    {
+                        if (emails[i].GetFullName() == command)
+                        {
+                            if (i + 1 == emails.size())
+                            {
+                                emails.pop_back();
+                            }
+                            else{
+                                emails[i] = emails[emails.size()-1];
+                                emails.pop_back();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         else if (command == "ls"){
             PrintAllMails();
         }
